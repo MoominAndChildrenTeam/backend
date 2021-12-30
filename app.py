d09fa2702b33e69aef4b75fff77e886cf7c5dbc1
HANJANGWON <wkddnjsdl00@naver.com>
2021-12-30 15:43:13 +0900

    Revert "test"
    This reverts commit edf7df685f63649d261ef9a91378e38eefa25411.

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
+from flask import Flask, render_template, request, jsonify
+import requests
 from datetime import datetime
-
-import gridfs
-import io
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask import send_file
 from pymongo import MongoClient
+
+
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.sylvm.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.moomin
@@ -12,48 +11,48 @@ db = client.moomin
 app = Flask(__name__)
 
 
-# main
+
+#main
 @app.route('/')
 @app.route('/main_page')
 def mainpage():
     return render_template('index.html')
 
-## 모든 피드를 불러옴
 @app.route("/mainpage", methods=["GET"])
 def all_feed_get():
     all_feed_list = list(db.all_feeds.find({}, {'_id': False}))
     return jsonify({'all_feeds': all_feed_list})
 
 
-# mypage
+
+
+#mypage
 @app.route('/my_page')
 def my_page():
     return render_template('mypage.html')
 
-## 내가 쓴 글만 불러옴
+
 @app.route("/mypage", methods=["GET"])
 def feed_get():
-    feed_list = list(db.my_feeds.find({}, {'_id': False}))
+    feed_list = list(db.my_feeds.find({ }, {'_id': False}))
     return jsonify({'my_feeds': feed_list})
 
-
-# upload
+#upload
 @app.route('/upload_page')
 def upload_page():
-    return render_template("apply_photo.html")
+    return render_template('upload.html')
 
 
-# 게시물 업로드(날짜, 이름, 코멘트, 좋아요)
 @app.route('/upload', methods=["POST"])
 def upload():
     date = datetime.today().strftime("%m{} %d{}")
     date = date.format('월', '일')
-    name_receive = request.form['name_give']
+    image_receive = request.form['image_give']
     comment_receive = request.form['comment_give']
     like = False
     doc = {
         'date': date,
-        'name': name_receive,
+        'image': image_receive,
         'comment': comment_receive,
         'like': like
     }
@@ -62,31 +61,23 @@ def upload():
     return jsonify({'msg': '저장 완료'})
 
 
-# 이미지파일 업로드
-@app.route('/upload_done', methods=["POST"])
-def upload_done():
-    file_user_receive = request.form['file_user']
-    uploaded_files = request.files["file"]
-    extension = uploaded_files.filename.split('.')[-1]
-    mytime = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
-    filename = f'{file_user_receive} - {mytime}'
-    save_to = f'static/{filename}.{extension}'
-    uploaded_files.save(save_to)
-
-    doc = {'user':file_user_receive, 'img': f'{filename}.{extension}'}
-    db.img.insert_one(doc)
-
-
-    return render_template('upload.html')
-
+# #users
+# @app.route("/users", methods=['POST'])
+# def create_user():
+#     user = {
+#         'name':request.form["name"],
+#         'id':request.form["id"]
+#     }
+#     db.users.insert_one(user)
 
 # like/unlike
 @app.route('/mainpage', methods=["POST"])
 def set_like():
     like_receive = request.form['like_give']
-    db.all_feeds.update_one({'name': '한장원'}, {'$set': {'like': like_receive}})  ## 로그인된 유저네임을 받아와야 함
+    db.all_feeds.update_one({'image': '한장원'}, {'$set': {'like': like_receive}})
     print(like_receive)
     return jsonify({'msg': '좋아요'})
+
 
 
 if __name__ == '__main__':
